c2866249a606e272ae24eca44b8415fc6a9a82e8
Tj240 <144957896+Tj240@users.noreply.github.com>
2023-09-17 04:49:50 +0000

    extra chatbot features

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-
-
 print('Welcome to Elite 101 Chatbot')
-
-
+print()
 # gets user inputs
 name = input("what is your name? ")
+print()
 age = input("how old are you ")
 print("")
 
 # uses user input to print information
 print("Hello " +  name  + " !!!")
+print()
 print("only " + age + " still got your whole life ahead of you")
 print("")
-day = str(input("how's your day "))
+day = str(input("how's your day? "))
+print()
 if (day == 'good'):
   print("im glad your day's going well")
-  
 else:
   print('well i hope i make it better')
   
@@ -24,25 +23,50 @@ print()
 print('please choose following options. ')
 
 while True:
-  print("1. your favorite shoe brand")
-  print("2. end chat")
+  print("1. your favorite shoe brand.")
+  print("2. what's your favorite phone brand.")
+  print("3. if you play games what's your preferred console.")
+  print("4. end chat")
   
   choice = (input('enter choice: '))
 
   choice = choice.strip()
   if (choice == '1'):
     brand = str(input('what is your favorite brand? '))
-    if brand.lower()=='jordans' or brand.lower()=='js':
-      print('wow i like jordans too')
-      break
+    if brand.lower()=='jordans' or brand.lower()=='js'or brand.lower()=='jordan':
+      print()
+      print('wow i like jordans too!!!')
+      print()
     else:
+      print()
       print('oh thats a good choice for me my favorite is jordans')
-      break
+      print()
   elif (choice =='2'):
+    phone = str(input("what's your favorite phone brand. "))
+    if phone.lower()=='samsung':
+      print()
+      print('wow!!! I like samsung too')
+      print()
+    else:
+      print()
+      print('dang really for me its samsung')
+      print()
+  elif (choice =='3'):
+    game = str(input("if you play games what's your preferred console. "))
+    if game.lower()=='xbox':
+      print()
+      print('crazy thats also my favorite console!!!')
+      print()
+    else:
+      print()
+      print('not a bad choice for me its xbox')
+      print()
+  elif (choice =='4'):
     exit('end')
+      
     break
   else:
-    print('please enter options 1-2')
+    print('please enter options 1-4')
 
 
  
