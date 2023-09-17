f1141c91dcd42e46cbb346c7b8dbc4847bc05267
Tj240 <144957896+Tj240@users.noreply.github.com>
2023-09-17 04:03:44 +0000

    chat

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print('hello world!!!')
+
+
+print('Welcome to Elite 101 Chatbot')
+
+
+# gets user inputs
+name = input("what is your name? ")
+age = input("how old are you ")
+print("")
+
+# uses user input to print information
+print("Hello " +  name  + " !!!")
+print("only " + age + " still got your whole life ahead of you")
+print("")
+day = str(input("how's your day "))
+if (day == 'good'):
+  print("im glad your day's going well")
+  
+else:
+  print('well i hope i make it better')
+  
+choice = ""
+print()
+print('please choose following options. ')
+
+while True:
+  print("1. your favorite shoe brand")
+  print("2. end chat")
+  
+  choice = (input('enter choice: '))
+
+  choice = choice.strip()
+  if (choice == '1'):
+    brand = str(input('what is your favorite brand? '))
+    if brand.lower()=='jordans' or brand.lower()=='js':
+      print('wow i like jordans too')
+      break
+    else:
+      print('oh thats a good choice for me my favorite is jordans')
+      break
+  elif (choice =='2'):
+    exit('end')
+    break
+  else:
+    print('please enter options 1-2')
+
+
+ 
